eb56e920d69a649657f8a7c584f282c861a09750
Mathilde Wattrelos <mat.wattrelos@yahoo.com>
2021-09-13 12:30:07 +0100

    Update assets

--- src/Experience/assets.js
+++ src/Experience/assets.js
@@ -7,15 +7,16 @@ export default [
                 { name: 'dataStanton', source: '/assets/stanton.json', type: 'data' },
                 { name: 'starsTexture', source: '/assets/stars.png', type: 'texture' },
                 { name: 'asteroidTexture', source: '/assets/asteroid.png', type: 'texture' },
-                { name: 'aberdeenTexture', source: '/assets/aberdeen.jpg', type: 'texture' },
+                { name: 'aberdeenTexture', source: '/assets/aberdeen.png', type: 'texture' },
                 { name: 'arccorpTexture', source: '/assets/arccorp.jpg', type: 'texture' },
-                { name: 'arialTexture', source: '/assets/arial.jpg', type: 'texture' },
-                { name: 'crusaderTexture', source: '/assets/crusader2.jpg', type: 'texture' },
-                { name: 'daymarTexture', source: '/assets/daymar.jpg', type: 'texture' },
+                { name: 'arialTexture', source: '/assets/arial.png', type: 'texture' },
+                { name: 'crusaderTexture', source: '/assets/crusader.png', type: 'texture' },
+                { name: 'daymarTexture', source: '/assets/daymar2.png', type: 'texture' },
+                { name: 'cellinTexture', source: '/assets/cellin2.png', type: 'texture' },
+                { name: 'yelaTexture', source: '/assets/yela2.png', type: 'texture' },
                 { name: 'hurstonTexture', source: '/assets/hurston2.jpg', type: 'texture' },
-                { name: 'itaTexture', source: '/assets/ita.jpg', type: 'texture' },
+                { name: 'magdaTexture', source: '/assets/magda.png', type: 'texture' },
                 { name: 'microtechTexture', source: '/assets/microtech.jpg', type: 'texture' },
-                { name: 'yelaTexture', source: '/assets/yela.jpg', type: 'texture' },
             ]
     }
 ]
