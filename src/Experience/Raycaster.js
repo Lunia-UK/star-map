d0ee0c8fcb8fc7fb0613b05ddbb9624245f6905c
Mathilde Wattrelos <mat.wattrelos@yahoo.com>
2021-10-15 09:20:52 +0100

    Add focus on Jurisdiction

--- src/Experience/Raycaster.js
+++ src/Experience/Raycaster.js
@@ -11,6 +11,7 @@ export default class Raycaster {
         this.sizes = this.experience.sizes
         this.objectSelected = this.experience.objectSelected
         this.objectSelectedPosition = null
+        this.currentLevel = 'System'
         this.camera = this.experience.camera
         this.controls = this.camera.modes.debug.orbitControls
         this.resources = this.experience.resources
@@ -25,16 +26,17 @@ export default class Raycaster {
         this.mouse = new THREE.Vector2()
         window.addEventListener('mousemove', (event) => {
             this.mouse.x = event.clientX / this.sizes.width * 2 - 1
-            this.mouse.y = - (event.clientY / this.sizes.height) * 2 + 1
+            this.mouse.y = -(event.clientY / this.sizes.height) * 2 + 1
         })
     }
+
     setRaycaster() {
         this.raycaster = new THREE.Raycaster()
         this.time.on('tick', () => {
             this.raycaster.setFromCamera(this.mouse, this.camera.instance)
             this.intersects = this.raycaster.intersectObjects(this.objectToTest);
-            for(const intersect of this.intersects) {
-                if(this.intersects.length && this.objectSelected !== this.intersects[0].object) {
+            for (const intersect of this.intersects) {
+                if (this.intersects.length) {
                     this.currentIntersect = this.intersects[0]
                     this.currentOrbit = intersect.object.parent.children[1]
                     this.currentOrbit.material.color = new THREE.Color(this.currentOrbit.focusColor)
@@ -42,10 +44,10 @@ export default class Raycaster {
                     this.currentIntersect = null
                 }
             }
-            for(const object of this.objectToTest) {
-                if(!this.intersects.find(intersect => intersect.object === object)) {
+            for (const object of this.objectToTest) {
+                if (!this.intersects.find(intersect => intersect.object === object)) {
                     this.currentOrbit = object.parent.children[1]
-                    if(object !== this.objectSelected){
+                    if (object !== this.objectSelected) {
                         this.currentOrbit.material.color = new THREE.Color(this.currentOrbit.color)
                     }
                 }
@@ -53,7 +55,7 @@ export default class Raycaster {
 
         })
         this.resources.on('groupEnd', (_group) => {
-            if(_group.name === 'base') {
+            if (_group.name === 'base') {
                 this.dblClick();
                 this.controlsChange();
             }
@@ -63,15 +65,15 @@ export default class Raycaster {
 
     controlsChange() {
         this.controls.addEventListener('change', () => {
-            if(this.objectSelected && !this.animationInProgress) {
+            if (this.objectSelected && !this.animationInProgress) {
                 this.distance = this.camera.instance.position.distanceTo(this.objectSelectedPosition);
                 this.objectSelected.objectType !== 'Star' ? this.limiteDistance = 20 : this.limiteDistance = 100
 
-                console.log(this.limiteDistance)
                 if (this.distance > this.limiteDistance) {
+                    this.currentLevel = 'System'
                     this.objectSelected = null
                     this.experience.objectSelected = null
-                    this.infoElements[0].innerText = 'System'
+                    this.infoElements[0].innerText = this.currentLevel
                     this.infoElements[1].innerText = 'Stanton'
                     this.infoElements[2].innerText = `${this.resources.items.dataStanton.coords.lat.toPrecision(8)}, ${this.resources.items.dataStanton.coords.long.toPrecision(8)}`
                     this.infoElements[3].innerText = this.resources.items.dataStanton.area
@@ -84,7 +86,7 @@ export default class Raycaster {
         window.addEventListener(
             "dblclick",
             () => {
-                if(this.currentIntersect && this.objectSelected !== this.currentIntersect.object) {
+                if (this.currentIntersect) {
                     this.objectSelected = this.currentIntersect.object;
                     this.experience.objectSelected = this.objectSelected
                     this.animationInProgress = true
@@ -94,11 +96,13 @@ export default class Raycaster {
 
                         case 'Moon':
 
+                            this.currentLevel = 'Moon'
+
                             // Moon position
                             this.x = this.currentIntersect.object.parent.position.x + this.currentIntersect.object.parent.parent.position.x
                             this.y = this.currentIntersect.object.parent.position.y + this.currentIntersect.object.parent.parent.position.y
                             this.z = this.currentIntersect.object.parent.position.z + this.currentIntersect.object.parent.parent.position.z
-                            this.objectSelectedPosition =  new THREE.Vector3( this.x, this.y, this.z )
+                            this.objectSelectedPosition = new THREE.Vector3(this.x, this.y, this.z)
 
                             // Distance camera/moon
                             this.distanceFocusZ = 0.5
@@ -110,17 +114,67 @@ export default class Raycaster {
                             this.audioClick.volume = 0.8
 
                             break;
+
                         case 'Planet':
+                            switch (this.currentLevel) {
 
-                            // Planet position
-                            this.x = this.currentIntersect.object.parent.position.x
-                            this.y = this.currentIntersect.object.parent.position.y
-                            this.z = this.currentIntersect.object.parent.position.z
-                            this.objectSelectedPosition =  new THREE.Vector3( this.x, this.y, this.z )
+                                case 'System':
+                                    this.currentLevel = 'Jurisdiction'
+                                    // Jurisdiction position
+                                    this.x = this.currentIntersect.object.parent.position.x
+                                    this.y = this.currentIntersect.object.parent.position.y
+                                    this.z = this.currentIntersect.object.parent.position.z
+                                    this.objectSelectedPosition = new THREE.Vector3(this.x, this.y, this.z)
 
-                            // Distance camera/Planet
-                            this.distanceFocusZ = 5
-                            this.distanceFocusY = 0.75
+                                    // Distance camera/Jurisdiction
+                                    this.distanceFocusZ = 5
+                                    this.distanceFocusY = 0.75
+                                    break;
+
+                                case 'Jurisdiction':
+                                    if(this.objectSelected === this.objectSelected) {
+                                        this.currentLevel = 'Planet'
+                                    }
+                                    // Planet position
+                                    this.x = this.currentIntersect.object.parent.position.x
+                                    this.y = this.currentIntersect.object.parent.position.y
+                                    this.z = this.currentIntersect.object.parent.position.z
+                                    this.objectSelectedPosition = new THREE.Vector3(this.x, this.y, this.z)
+
+                                    // Distance camera/Planet
+                                    this.distanceFocusZ = 3
+                                    this.distanceFocusY = 0.75
+                                    break;
+
+                                case 'Planet':
+                                    if(this.objectSelected === this.objectSelected) {
+                                        console.log('hello')
+                                        this.currentLevel = 'Jurisdiction'
+                                    }
+                                    // Jurisdiction position
+                                    this.x = this.currentIntersect.object.parent.position.x
+                                    this.y = this.currentIntersect.object.parent.position.y
+                                    this.z = this.currentIntersect.object.parent.position.z
+                                    this.objectSelectedPosition = new THREE.Vector3(this.x, this.y, this.z)
+
+                                    // Distance camera/Jurisdiction
+                                    this.distanceFocusZ = 5
+                                    this.distanceFocusY = 0.75
+                                    break;
+
+                                case 'Moon':
+                                    this.currentLevel = 'Jurisdiction'
+                                    // Jurisdiction position
+                                    this.x = this.currentIntersect.object.parent.position.x
+                                    this.y = this.currentIntersect.object.parent.position.y
+                                    this.z = this.currentIntersect.object.parent.position.z
+                                    this.objectSelectedPosition = new THREE.Vector3(this.x, this.y, this.z)
+
+                                    // Distance camera/Jurisdiction
+                                    this.distanceFocusZ = 5
+                                    this.distanceFocusY = 0.75
+                                    break;
+                            }
 
                             // Audio
                             this.audioClick = new Audio('../sounds/click_v3.mp3');
@@ -135,7 +189,7 @@ export default class Raycaster {
                             this.x = this.currentIntersect.object.position.x
                             this.y = this.currentIntersect.object.position.y
                             this.z = this.currentIntersect.object.position.z
-                            this.objectSelectedPosition =  new THREE.Vector3( this.x, this.y, this.z )
+                            this.objectSelectedPosition = new THREE.Vector3(this.x, this.y, this.z)
 
                             // Distance camera/Planet
                             this.distanceFocusZ = 80
@@ -146,8 +200,12 @@ export default class Raycaster {
                             break;
                     }
                     this.camera.modes.debug.orbitControls.target = this.objectSelectedPosition
-                    this.infoElements[0].innerText = this.objectSelected.objectType
-                    this.infoElements[1].innerText = this.objectSelected.name
+                    this.infoElements[0].innerText = this.currentLevel
+                    if (this.currentLevel === 'Planet') {
+                        this.infoElements[1].innerText = this.objectSelected.name
+                    } else {
+                        this.infoElements[1].innerText = this.objectSelected.parent.name
+                    }
                     this.infoElements[2].innerText = `${this.objectSelected.parent.position.x.toPrecision(8)}, ${this.objectSelected.parent.position.z.toPrecision(8)}`
                     this.infoElements[3].innerText = this.objectSelected.area
 
